e121729114a5ec564ecac489685c541d87bf7729
CurdtMillion <curtcecil@gmail.com>
2020-08-06 16:50:31 -0500

    added one more user and tweet to twitoff.sqlite

--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -6,16 +6,20 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     username = db.Column(db.String(80), unique=True, nullable=False)
     follower_count = db.Column(db.Integer, nullable=False)
+    #tweet = db.relationship('Tweet', uselist=False, back_populates='user')
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    #tweet_id = db.Column(db.Integer, db.ForeignKey('tweet.id'))
     text = db.Column(db.Unicode(300))
-    #user = db.relationship('User', backref=db.backref('tweet', lazy=True))
+    #user = db.relationship('User', back_populates='tweet')
     
     def __repr__(self):
         return '<Tweet %r>' % self.text
+
