e1b39b944aa1878a2c04ef300e34aae2c227d033
CurdtMillion <curtcecil@gmail.com>
2020-08-12 18:46:17 -0500

    Update for Unt 3 Module 3

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from .db_model import db, User
 from .twitter import add_user_tweepy
+from .predict import predict_user
 
 def create_app():
     '''Create and configure an instance of the Flask application'''
@@ -27,5 +28,22 @@ def create_app():
             message = "Error adding {}: {}".format(name, e)
             tweets = []
         return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user1, user2, tweet_text)
+
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                tweet_text, user1 if prediction else user2, user2 if prediction else user1
+            )
+
+        return render_template('prediction.html', title='Prediction', message=message)
 
     return app
---
+++ twitoff/predict.py
@@ -0,0 +1,31 @@
+'''Prediction of Users based on Tweet embeddings'''
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from .db_model import User
+from .twitter import BASILICA
+
+
+def predict_user(user1, user2, tweet_text):
+    '''Determine and return which user is more likely to say a given tweet.
+    # Arguments:
+        user1: str, twitter user name for user 1 in comparison from web form
+        user2: str, twitter user name for user 1 in comparison from web form
+        tweet_text: str, tweet text to evaluate
+    # Returns:
+        prediction from logistic regression model
+    '''
+    user1 = User.query.filter(User.username == user1).one()
+    user2 = User.query.filter(User.username == user2).one()
+    user1_embeddings = np.array([tweet.embedding for tweet in user1.tweet])
+    user2_embeddings = np.array([tweet.embedding for tweet in user2.tweet])
+    
+    # Combine embeddings and create labels
+    embeddings = np.vstack([user1_embeddings, user2_embeddings])
+    labels = np.concatenate([np.ones(len(user1_embeddings)), 
+                             np.zeros(len(user2_embeddings))])
+
+    # Train model and convert input text to embedding
+    lr = LogisticRegression().fit(embeddings, labels)
+    tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
+
+    return lr.predict([tweet_embedding])[0]
